8bfb82bd30062cf5c1f89d696a443336cd236e76
马华健 <mhj1631@muyingzhijia.com>
2015-12-28 10:26:47 +0800

    Initial Commit

---
+++ RuntimeDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeDemo
+//
+//  Created by Anc on 15/12/28.
+//  Copyright © 2015年 Ancc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
